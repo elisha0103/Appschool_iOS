c526d06a62ecbfe62e127bd95dbca65f63e7ba3f
TaeYoung Jin <elisha0103@naver.com>
2022-10-17 15:28:27 +0900

    change button design, fix bug
    change button design and fix bug
    add '%' operator

--- Calc-twoNumber/Calc-twoNumber/ViewController.swift
+++ Calc-twoNumber/Calc-twoNumber/ViewController.swift
@@ -30,17 +30,32 @@ class ViewController: UIViewController {
     @IBAction func pressOpButton(_ sender: UIButton) {  // 연산자 버튼
         op = sender.titleLabel?.text ?? "" // 연산자 기호 누를 때 op 변수에 저장됨
         resultLabelField.text = "0"  // 연산자를 누르는 타이밍은 number1 입력받고 누르기 때문에 number2 입력받기 위해서 resultLabel은 다시 0이 표시되도록 한다.
-        number1 = tmpNumber  // 연산자를 눌렀다는 얘기는 첫번째 피연산자의 입력이 끝났다는 얘기. 임시 tmp변수에 있는 문자열을 number1에 저장해준다.
+        if number1 == "" {
+            number1 = tmpNumber  // 연산자를 눌렀다는 얘기는 첫번째 피연산자의 입력이 끝났다는 얘기. 임시 tmp변수에 있는 문자열을 number1에 저장해준다.
+        }
+        
+        if number2 != "" {
+            number2 = tmpNumber
+        }
         tmpNumber = ""  // 다시 두번째 피연산자를 받을 준비를 한다.
     }
     
     @IBAction func pressResultButton(_ sender: UIButton) {
-        number2 = tmpNumber  // '=' 기호를 눌렀으면 두번째 피연산자의 입력도 끝났다는 의미
+        if sender.titleLabel?.text == "%"{
+            op = "%"
+        }
+        
+        if number2 == "" {
+            number2 = tmpNumber  // '=' 기호를 눌렀으면 두번째 피연산자의 입력도 끝났다는 의미
+        }
         numberSet = NumberSet(Float(number1) ?? 0, Float(number2) ?? 0, op ?? "No data") // 피연산자, 연산자를 저장하고 안에서 결과 값까지 계산해서 저장해두는 구조체 타입 변수 초기화
+        
         if let resultNumber = numberSet?.result, let iop = op{  // 옵셔널 바인딩
             resultLabelField.text = resultNumber  // 결과 창에 연산 결과 출력
             historyArray.append("\(number1) \(iop) \(number2) = \(resultNumber)") // 계산 기록을 담는 배열에 연산기록
             historyArray = historyPrint(arrayString: &historyArray, printLabel: historyLabel) // 계산 기록을 출력하는 함수에 배열 매개변수와 라벨타입의 매개변수를 넘겨주고, 계산 기록 라벨에 배열 출력. 그리고 출력 후 갱신된 배열 반환
+            
+            number1 = resultNumber
         }
         tmpNumber = "" // 다음 계산을 위한 준비
     }
@@ -58,4 +73,3 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 }
-
--- Calc-twoNumber/NumberSetClass.swift
+++ Calc-twoNumber/NumberSetClass.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 진태영 on 2022/10/14.
 //
-
 import Foundation
 
 struct NumberSet{
@@ -12,7 +11,6 @@ struct NumberSet{
     let number2: Float  // 두번째 피연산자
     let op: String      // 연산자
     var result: String = ""  // 초기화 후 계산된
-
     
     init (_ number1: Float, _ number2: Float, _ op: String){
         self.number1 = number1  // 첫 번째 피연산자 저장
@@ -34,11 +32,12 @@ struct NumberSet{
                 break
             }
             self.result = "\(self.number1 / self.number2)"
+        case "%":
+            self.result = "\(self.number2 * 0.01)"
+            
         default:  // op 선택 없이 숫자만 누르고 '=' 누를경우 출력할 문구
             self.result = "Select OP"
             break
         }
     }
 }
-
-
