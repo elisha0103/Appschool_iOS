4ff5458292592c48c9762fd0967a5def7d4f7836
TaeYoung Jin <elisha0103@naver.com>
2022-10-31 10:28:36 +0900

    Animation method

--- HelloSwiftUI/HelloSwiftUI/AnimationDemo.swift
+++ HelloSwiftUI/HelloSwiftUI/AnimationDemo.swift
@@ -12,6 +12,21 @@ struct AnimationDemo: View {
     @State private var scale: CGFloat = 1.0
     var body: some View {
         VStack{
+            
+            // 명시적 애니메이션 선언방법 - withAnimation 클로저 사용
+            Button(action: {
+                withAnimation(.linear(duration: 1).repeatForever(autoreverses: true)) {
+                    rotation = (rotation < 360) ? rotation + 60 : 0
+                }
+                scale = (scale < 2.8) ? scale + 0.3 : 1
+            }) {
+                Text("Click to animate")
+                    .scaleEffect(scale)
+                    .rotationEffect(.degrees(rotation))
+            }
+            
+            // 암묵적 애니메이션 선언방법
+            /*
             Button(action: {
                 rotation = (rotation < 360) ? rotation + 60 : 0
                 scale = (scale < 2.8) ? scale + 0.3 : 1.0
@@ -19,8 +34,14 @@ struct AnimationDemo: View {
                 Text("Click to animate")
                     .scaleEffect(scale)
                     .rotationEffect(.degrees(rotation))
-                    .animation(.linear, value: rotation)
+                // 애니메이션 기본
+                    .animation(.linear, value: scale)
+                // 무한 반복 애니메이션
+//                    .animation(.linear(duration: 1).repeatForever(autoreverses: true), value: scale)
+                // 스프링 애니메이션
+//                    .animation(.spring(response: 1, dampingFraction: 0.5, blendDuration: 0), value: scale)
             }
+             */
         }
         .padding()
     }
