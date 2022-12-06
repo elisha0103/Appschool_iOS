4e846a86f2c59866d3ef1a1b7749f6742eace71e
TaeYoung Jin <elisha0103@naver.com>
2022-12-06 17:18:39 +0900

    carStore Firebase

---
+++ FirebaseExample/FirebaseExample/FirebaseExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseExampleApp.swift
+//  FirebaseExample
+//
+//  Created by 진태영 on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
