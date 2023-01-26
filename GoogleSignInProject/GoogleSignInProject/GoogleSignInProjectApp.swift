a7b65c1278d93d7965d584224b19c823acae63a7
Taeyoung Jin <elisha0103@naver.com>
2023-01-26 10:32:49 +0900

    [Feat]: 구글 로그인 예제

---
+++ GoogleSignInProject/GoogleSignInProject/GoogleSignInProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSignInProjectApp.swift
+//  GoogleSignInProject
+//
+//  Created by 진태영 on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSignInProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
