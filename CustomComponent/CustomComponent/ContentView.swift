c08a069e7591e97c4e7c731aa7edf4f1556307fd
Taeyoung Jin <elisha0103@naver.com>
2023-01-26 10:33:34 +0900

    [Feat]: 커스텀 컴포넌트 프로젝트 수정

--- CustomComponent/CustomComponent/ContentView.swift
+++ CustomComponent/CustomComponent/ContentView.swift
@@ -9,31 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showsCustomAlert = false
-    @State private var text: String = ""
-    @State private var showsSystemAlert = false
 
     var body: some View {
         VStack {
-            CustomTextField(
-              text: $text,
-              placeholder: "플레이스 홀더",
-              maximumCount: 5)
-
-            Button(action: { showsSystemAlert.toggle() }) {
-              Text("시스템 Alert 진행시켜!")
-            }
-            .buttonStyle(.reative)
-
-            Button(action: { }) {
-              Text("안녕")
-            }
-            .buttonStyle(.reative)
-
             Button(action: { showsCustomAlert.toggle() }) {
               Text("커스텀 Alert 진행시켜!")
                 .foregroundColor(.cyan)
             }
-            .buttonStyle(.reative)
           .padding()
           .customAlert(
             isPresented: $showsCustomAlert,
