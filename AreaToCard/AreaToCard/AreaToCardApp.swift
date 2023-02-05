5e0f4aabc0fa4196f07e31ccb8476f16c0133120
Taeyoung Jin <elisha0103@naver.com>
2023-02-05 23:34:51 +0900

    확장, 축소 애니메이션

---
+++ AreaToCard/AreaToCard/AreaToCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaToCardApp.swift
+//  AreaToCard
+//
+//  Created by 진태영 on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct AreaToCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
