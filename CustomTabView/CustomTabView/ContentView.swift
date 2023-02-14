cc485186fbeae4f021bee61a678f94555fd0fb95
Taeyoung Jin <elisha0103@naver.com>
2023-02-14 15:27:16 +0900

    커스텀 탭뷰 추가

---
+++ CustomTabView/CustomTabView/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  CustomTabView
+//
+//  Created by 진태영 on 2023/02/14.
+//
+
+import SwiftUI
+
+let backgroundColor = Color.init(white: 0.92)
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            backgroundColor
+                .ignoresSafeArea()
+            VStack(spacing: 70) {
+                TabBarView1()
+                
+                TabBarView2()
+                
+                TabBarView3()
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+enum Tab: Int, Identifiable, CaseIterable, Comparable {
+    static func < (lhs: Tab, rhs: Tab) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+    
+    case home, game, apps, movie
+    
+    internal var id: Int { rawValue }
+    
+    var icon: String {
+        switch self {
+        case .home:
+            return "house.fill"
+        case .game:
+            return "gamecontroller.fill"
+        case .apps:
+            return "square.stack.3d.up.fill"
+        case .movie:
+            return "play.tv.fill"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .home:
+            return "Home"
+        case .game:
+            return "Games"
+        case .apps:
+            return "Apps"
+        case .movie:
+            return "Movies"
+        }
+    }
+    
+    var color: Color {
+        switch self {
+        case .home:
+            return .indigo
+        case .game:
+            return .pink
+        case .apps:
+            return .orange
+        case .movie:
+            return .teal
+        }
+    }
+}
---
+++ CustomTabView/CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by 진태영 on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
